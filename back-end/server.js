c544473146b136307f83019811ffef5a62ef94ad
Andrey <adahorapirola@gmail.com>
2024-05-23 13:48:35 -0300

    Preparação do Ambiente de Produção (FrontEnd e BackEnd)

---
+++ back-end/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import authRoutes from './routes/auth.routes.js';
+import dotenv from "dotenv";
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+console.log(process.env.MONGO_URI);
+
+app.use("/api/auth", authRoutes);   
+
+
+app.listen(PORT, () => {
+    console.log(`Server está funcionando na porta ${PORT}`);
+    connectMongoDB();
+});
