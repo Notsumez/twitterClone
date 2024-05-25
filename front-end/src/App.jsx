eff216179757c3ab2ad075d950af9549ece1c9e0
Andrey <adahorapirola@gmail.com>
2024-05-25 17:40:58 -0300

    DaisyUI instalado

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -8,7 +8,13 @@ function App() {
 
   return (
     <>
-      <p className='text-5xl text-red-600 bg-blue-600'>Teste 1 2 3</p>
+      <button className="btn">Button</button>
+<button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-ghost">Ghost</button>
+<button className="btn btn-link">Link</button>
     </>
   )
 }
